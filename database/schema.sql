567181112f2c4de400a0c25b24e1770d6ccbd116
shuh <nonya@biz.com>
2019-07-06 13:23:10 -0400

    database for venue and artists

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-### Schema
+### Schema - Venue
 DROP DATABASE IF EXISTS venue_db;
 CREATE DATABASE venue_db;
 USE venue_db;
@@ -7,6 +7,26 @@ CREATE TABLE venues
 (
 	id int NOT NULL AUTO_INCREMENT,
 	venueName varchar(255) NOT NULL,
-    venueMembers int (2) NOT NULL,
+    venueLocation varchar (255) NOT NULL,
+    venueManager varchar (255) NOT NULL,
+    venuePhone int (9) NOT NULL,
+    venueEmail varchar (255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+### Schema - Artist
+DROP DATABASE IF EXISTS artist_db;
+CREATE DATABASE artist_db;
+USE artist_db;
+
+CREATE TABLE artists
+(
+	id int NOT NULL AUTO_INCREMENT,
+	artistName varchar(255) NOT NULL,
+    artistLocation varchar (255) NOT NULL,
+    artistMembers int (9) NOT NULL,
+    artistManager varchar (255),
+    artistPhone int (9) NOT NULL,
+    artistEmail varchar (255) NOT NULL,
 	PRIMARY KEY (id)
 );
