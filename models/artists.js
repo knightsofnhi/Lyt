dd166a497b4c25302020154039bb5a2a9b4db318
shuh <nonya@biz.com>
2019-07-11 17:12:06 -0400

    using sequelize 10 for example

--- models/artists.js
+++ models/artists.js
@@ -1,15 +1,21 @@
 module.exports = function(sequelize, DataTypes) {
-  const Artists = sequelize.define("Artists", {
-    artistName: DataTypes.STRING,
-    artistMembers: DataTypes.STRING,
-    artistLocation: DataTypes.STRING,
-    artistContactName: DataTypes.STRING,
-    artistContactPhone: DataTypes.STRING,
-    artistContactEmail: DataTypes.STRING
+  const Artist = sequelize.define("Artist", {
+    text: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a Artist from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our Artist is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    },
+    complete: {
+      type: DataTypes.BOOLEAN,
+      // defaultValue is a flag that defaults a new Artists complete value to false if
+      // it isn't supplied one
+      defaultValue: false
+    }
   });
-  return Artists;
+  return Artist;
 };
-
-// module.exports = function(sequelize, DataTypes) {
-//   const Artists = sequelize.define("Artists", {
-//     artistName: DataTypes.STRING,
