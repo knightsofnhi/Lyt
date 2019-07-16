e41c75cf79a273bd07315c1b4429e9dc6f8f5a9d
shuh <nonya@biz.com>
2019-07-16 16:29:01 -0400

    app.get

--- public/js/artist-signup.js
+++ public/js/artist-signup.js
@@ -1,3 +1,8 @@
+app.get("/", function (req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+
+});
+
 $(document).ready(function() {
   // Getting a reference to the input field where user adds a new todo
   debugger;
